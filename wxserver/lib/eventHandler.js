2ae8bd2f9731339b26c5c10a69e3b726cb2a4faf
Jinhui Li <jinhui.li@pku.edu.cn>
2016-06-07 08:22:10 -0500

    wxserver: establish the framework of server
    features are as follows:
    1.Token is well managed now
    
    2.Got the menu with 2 main button
    each main button carries 3 subutton
    
    3.Handlers for text and event are defined now
    
    Signed-off-by: Jinhui Li <jinhui.li@pku.edu.cn>

---
+++ wxserver/lib/eventHandler.js
@@ -0,0 +1,34 @@
+var replyText = require('./reply.js')
+
+
+
+function eventHandler(reqbody,res)
+{
+  var body = reqbody.xml;
+  switch (body.Event[0])
+  {
+    case 'CLICK':
+      switch (body.EventKey[0])
+      {
+        case "V1001_RD":
+          var reply=replyText(reqbody,"您订阅了研发类实习信息");
+        break;
+        case "V1001_ML":
+          var reply=replyText(reqbody,"您订阅了机器学习类实习信息");
+        break;
+        case "V1001_FI":
+          var reply=replyText(reqbody,"您订阅了金融类实习信息");
+        break;
+      }
+      res.end(reply);
+      break;
+    case 'subscribe':
+        var reply=replyText(reqbody,"欢迎订阅软微实习小助手");
+        res.end(reply);
+      break;
+  }
+
+
+}
+
+module.exports = eventHandler;
---
+++ wxserver/lib/token.js
@@ -0,0 +1,51 @@
+var https = require('https');
+var fs = require('fs');
+
+var tokenFile = "./config/token"
+var tsFile = "./config/timestamp"
+const TWOHOUR = ( 3600 - 100 ) * 1000;
+
+token=""
+function getToken(callback)
+{
+  var lastTime = fs.readFileSync(tsFile);
+  var lastToken = fs.readFileSync(tokenFile);
+  if (Date.now()-lastTime < TWOHOUR)
+  {
+    callback(lastToken);
+    return lastToken;
+  }
+  tmpl = require("tmpl");
+  url="https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={APPID}&secret={APPSECRET}";
+  APPID="wx59e38f4f5e6cd31d";
+  APPSECRET="df8252d4e01422274dd5b7a9815a9246";
+  url=tmpl(url,{
+    APPID:APPID,
+    APPSECRET:APPSECRET
+  });
+  console.log(url);
+  https.get(url, function(res) {
+    console.log("Got response: " + res.statusCode);
+    chunk="";
+    res.on('data',function(data){
+      chunk+=data;
+    });
+
+    res.on('end',function(data){
+      token=JSON.parse(chunk).access_token;
+      console.log(token);
+      callback(token);
+      fs.writeFileSync(tokenFile,token);
+      fs.writeFileSync(tsFile,Date.now());
+      console.log(chunk);
+    });
+
+  });
+
+  setTimeout(getToken,7200*1000);
+  return -1;
+}
+
+
+
+module.exports = getToken;
